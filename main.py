1b63a475f9448b5f501af74830f14a16e2c6f1eb
ChevalierAlexis <al.chevalier@rns.ecolescreatives.com>
2022-05-06 08:26:18 +0200

    Implémentation classe carte

---
+++ main.py
@@ -0,0 +1,10 @@
+from msilib.schema import Class
+
+
+class Carte : 
+    def __init__ (self,mana,nom,description):
+        self.__mana =mana
+        self.__nom = nom
+        self.__des = description
+
+    
